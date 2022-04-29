29d9756adea142273e9947639a1b5336783a6fd6
Sanjay Chauhan <Chauhan.Sanjay@bcg.com>
2022-04-29 10:55:10 +0530

    context changes

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import sp from '../assets/spinner.gif'
+
+function Spinner() {
+  return (
+    <img
+      src={sp}
+      alt="Loading...."
+      style={{ width: '100px', margin: 'auto', display: 'block' }}
+    />
+  )
+}
+
+export default Spinner
